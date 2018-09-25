a229e1dd29955d5e30f5e23fb7c7732cf8134641
Kimberly Stafford <kimberlystafford@kimberlys-macbook-pro.local>
2018-09-25 12:55:42 -0400

    Initial Commit

---
+++ FractionCalc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FractionCalc
+//
+//  Created by Kimberly Stafford on 9/25/18.
+//  Copyright © 2018 Kimberly Stafford. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
